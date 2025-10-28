7dad7709f28b69534b68c2d896cf5394a24bab14
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2025-10-28 23:12:16 +0530

    cv link updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/11lpMnBSkXNtouoANeSvQicCZhXsiT5YT/view"
+            href="https://drive.google.com/file/d/11lpMnBSkXNtouoANeSvQicCZhXsiT5YT/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
